c6a13651d71ced4d3a3dd784e1704c8c0c3c4177
abolarin babatunde <abolarinbabatunde88@gmail.com>
2023-05-13 15:51:52 +0100

    save

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const video = document.getElementById('video')
-
+let model = `http://127.0.0.1:5500/models`
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri(model),
+  faceapi.nets.faceLandmark68Net.loadFromUri(model),
+  faceapi.nets.faceRecognitionNet.loadFromUri(model),
+  faceapi.nets.faceExpressionNet.loadFromUri(model)
+]).then(startVideo())
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  navigator.mediaDevices.getUserMedia(
+    { video: true }
+  ).then(stream => {
+    console.log(stream);
+    video.srcObject = stream
+    // err => console.error(err)
+    video.play()
+  })
 }
 
 video.addEventListener('play', () => {
@@ -21,7 +24,10 @@ video.addEventListener('play', () => {
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    .withFaceLandmarks()
+    .withFaceExpressions()
+    console.log(detections);
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
